1d318b77caca1ce99f793b3cf3b24b24b2abeff0
MrCorazon01 <khollemm@ept.sn>
2024-11-30 15:05:46 +0000

    Route for health check

--- app/routers/health.py
+++ app/routers/health.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 router = APIRouter()
 
-@router.get("/health")
-async def health_check():
-    return {"status": "OK"}
+@router.get("/health/ready")
+async def readiness():
+    return {"status": "ready"}
+
+@router.get("/health/live")
+async def liveness():
+    return {"status": "alive"}
